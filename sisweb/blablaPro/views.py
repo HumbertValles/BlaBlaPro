d200e3d034d8ae9a03bd5c6e1e211765226b5e9a
poltorres0999 <eichgammer@gmail.com>
2017-05-21 14:06:54 +0200

    added FavoriteTrip form, base.css and base.html both with bootstrap

--- sisweb/blablaPro/views.py
+++ sisweb/blablaPro/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from models import FavoriteTrip
 
 from django.shortcuts import render
 
 # Create your views here.
+def FavoriteTripView(request):
+    if request.method == 'POST':
+        favorite_trip_form = FavoriteTrip()
+        if favorite_trip_form.isvalid():
+            user = request.user
+            favorite_trip_form.user = user
+            favorite_trip_form.save()
--- sisweb/sisweb/settings.py
+++ sisweb/sisweb/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blablaPro.apps.BlablaproConfig',
+    'blablaPro',
 ]
 
 MIDDLEWARE = [
